9ad10b961547bb3da0a9b5f6dd0112063cff0f7f
Jagmeet Kaur <jagmeet.kaur@altudo.co>
2024-09-30 17:05:55 -0500

    links and image

--- src/components/cms/component/CTATileBlock/index.tsx
+++ src/components/cms/component/CTATileBlock/index.tsx
@@ -1,41 +1,98 @@
 import { CmsComponent } from "@remkoj/optimizely-cms-react";
-import { CTATileBlockDataFragment, CTATileBlockDataFragmentDoc} from "@/gql/graphql";
+import {
+  ButtonBlockPropertyDataFragment,
+  CTATileBlockDataFragment,
+  CTATileBlockDataFragmentDoc,
+} from "@/gql/graphql";
 import { CmsEditable } from "@remkoj/optimizely-cms-react/rsc";
 import { RichText } from "@remkoj/optimizely-cms-react/components";
+import CmsImage from "@/components/shared/cms_image";
+import ButtonBlock from "@/components/component/block/button_block";
+import button from "@/components/shared/button";
 
-
-export const CTATileBlockComponent : CmsComponent<CTATileBlockDataFragment> = ({ data, inEditMode }) => {
+export const CTATileBlockComponent: CmsComponent<CTATileBlockDataFragment> = ({
+  data,
+  inEditMode,
+}) => {
+  const buttonClasses: string[] = [];
   return (
-    <div
-    >
-      {/* <figure>
-        <EditableReactImage field={props?.fields?.BackgroundImage} />
-      </figure> */}
-      <div >
-        <div >
-        { (data.title || inEditMode) && <CmsEditable as="h2" cmsFieldName="CTATileTitle">{ data.title }</CmsEditable> }
-        { (data.description || inEditMode) && <CmsEditable as={RichText} cmsFieldName="CTATileDescription" text={ data.description?.json } /> }
-          <div >
-           {/*  {Boolean(props.fields.Link1.value.href) && (
-              <Button
-                variant={props?.fields?.CTAType1?.name}
-                label={props?.fields?.Link1}
-                buttonClassName={props.fields?.CTAColor1?.name}
+    <div>
+      <div className="article-image aspect-[5/2] md:aspect-[5/1] relative w-full lg:aspect-[3/1] lg:z-[-10] lg:shadow-xl">
+        <CmsImage
+          src={data.image}
+          alt="hero-image"
+          aria-hidden
+          priority
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div>
+        <div>
+          {(data.title || inEditMode) && (
+            <CmsEditable as="h2" cmsFieldName="CTATileTitle">
+              {data.title}
+            </CmsEditable>
+          )}
+          {(data.description || inEditMode) && (
+            <CmsEditable
+              as={RichText}
+              cmsFieldName="CTATileDescription"
+              text={data.description?.json}
+            />
+          )}
+          <div>
+            {
+              <CmsEditable
+                as={ButtonBlock}
+                cmsFieldName="Link1"
+                contentLink={{ key: null }}
+                data={{
+                  ...button,
+                  __typename: undefined, // Remove data type, so only data fields will be matched
+                  " $fragmentName": undefined, // Remove fragment source, so only data fields will be matched
+                  className:
+                    `${
+                      (
+                        button as
+                          | ButtonBlockPropertyDataFragment
+                          | undefined
+                          | null
+                      )?.className ?? ""
+                    } ${buttonClasses.join(" ")}`.trim() || undefined, // Apply additional classes
+                }}
               />
-            )}
-
-            {Boolean(props.fields.Link2.value.href) && (
-              <Button
-                variant={props?.fields?.CTAType2?.name}
-                label={props?.fields?.Link2}
-                buttonClassName={props.fields?.CTAColor2?.name}
+            }
+            {
+              <CmsEditable
+                as={ButtonBlock}
+                cmsFieldName="Link2"
+                contentLink={{ key: null }}
+                data={{
+                  ...button,
+                  __typename: undefined, // Remove data type, so only data fields will be matched
+                  " $fragmentName": undefined, // Remove fragment source, so only data fields will be matched
+                  className:
+                    `${
+                      (
+                        button as
+                          | ButtonBlockPropertyDataFragment
+                          | undefined
+                          | null
+                      )?.className ?? ""
+                    } ${buttonClasses.join(" ")}`.trim() || undefined, // Apply additional classes
+                }}
               />
-            )} */}
+            }
           </div>
         </div>
       </div>
     </div>
   );
-};CTATileBlockComponent.displayName = "CTATileBlock";
-CTATileBlockComponent.getDataFragment = () => ['CTATileBlockData', CTATileBlockDataFragmentDoc]
+};
+CTATileBlockComponent.displayName = "CTATileBlock";
+CTATileBlockComponent.getDataFragment = () => [
+  "CTATileBlockData",
+  CTATileBlockDataFragmentDoc,
+];
 export default CTATileBlockComponent;
