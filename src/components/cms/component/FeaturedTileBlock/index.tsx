f08c19e970ebf556038547160b5aa53f726e519c
Jagmeet Kaur <jagmeet.kaur@altudo.co>
2024-10-07 18:42:52 -0500

    error

--- src/components/cms/component/FeaturedTileBlock/index.tsx
+++ src/components/cms/component/FeaturedTileBlock/index.tsx
@@ -7,22 +7,20 @@ import React from "react";
 import Image from "next/image";
 import { CmsEditable } from "@remkoj/optimizely-cms-react/rsc";
 import { RichText } from "@remkoj/optimizely-cms-react/components";
+import CmsImage from "@/components/shared/cms_image";
 
 export const FeaturedTileBlockComponent: CmsComponent<
   FeaturedTileBlockDataFragment
 > = ({ data }) => {
   return data.Position === "Left" ? (
     <section className="relative w-full min-h-[560px] max-md:pr-5 max-md:max-w-full">
-      <Image
-        loading="lazy"
-        src={
-          typeof data?.backgroundImageSrc === "string"
-            ? data.backgroundImageSrc
-            : ""
-        }
-        alt="Background image for hero section"
-        className="object-cover absolute inset-0"
+      <CmsImage
+        src={data.backgroundImageSrc}
+        alt="hero-image"
+        aria-hidden
+        priority
         fill
+        className="object-cover absolute inset-0 max-md:max-w-full"
       />
       <div className="relative flex flex-col items-start px-20 py-36 max-w-full bg-white bg-opacity-70 w-[548px] max-md:px-5 max-md:py-24">
         <h1 className="self-stretch text-3xl uppercase text-teal-950 max-md:mr-2.5">
@@ -42,16 +40,13 @@ export const FeaturedTileBlockComponent: CmsComponent<
     </section>
   ) : (
     <section className="relative flex flex-col">
-      <Image
-        loading="lazy"
-        src={
-          typeof data?.backgroundImageSrc === "string"
-            ? data.backgroundImageSrc
-            : ""
-        }
-        alt="Background image for hero section"
-        className="z-0 w-full object-contain aspect-[2.57] max-md:max-w-full"
+      <CmsImage
+        src={data.backgroundImageSrc}
+        alt="hero-image"
+        aria-hidden
+        priority
         fill
+        className="object-contain z-0 w-full aspect-[2.57] max-md:max-w-full"
       />
       <div className="absolute inset-y-0 right-0 z-0 flex flex-col max-w-full left-[892px] w-[548px]">
         <div className="flex flex-col items-start px-14 py-36 bg-white bg-opacity-70 max-md:px-5 max-md:py-24 max-md:max-w-full">
@@ -60,7 +55,7 @@ export const FeaturedTileBlockComponent: CmsComponent<
             as={RichText}
             cmsFieldName="TileDescription"
             text={data.description?.json}
-            className="mt-10 text-base font-light text-teal-950"
+            className="z-0 w-full object-contain aspect-[2.57] max-md:max-w-full"
           />
           <button className="overflow-hidden px-12 py-6 mt-16 -mb-7 text-sm font-bold tracking-wide text-white uppercase bg-cyan-900 max-md:px-5 max-md:mt-10 max-md:mb-2.5">
             {data.buttontext}
