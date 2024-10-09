0fa3e16a20cf19dbbf3590241973d06bc150143f
Jagmeet Kaur <jagmeet.kaur@altudo.co>
2024-10-09 11:55:47 -0500

    vercel build

--- src/components/cms/component/HeroCarouselBlock/index.tsx
+++ src/components/cms/component/HeroCarouselBlock/index.tsx
@@ -8,9 +8,6 @@ import { CmsComponent } from "@remkoj/optimizely-cms-react";
 import { RichText } from "@remkoj/optimizely-cms-react/components";
 import { CmsEditable } from "@remkoj/optimizely-cms-react/rsc";
 import CmsImage from "@/components/shared/cms_image";
-import Slider, { Settings } from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export type BannerItems = Array<
   HeroBannerBlockDataFragment & IContentDataFragment
@@ -24,14 +21,6 @@ function filterMaybeArray<T>(
   return input.filter((x) => x) as Array<T>;
 }
 
-var settings: Settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-};
-
 export const HeroCarouselBlockComponent: CmsComponent<
   HeroCarouselBlockDataFragment
 > = ({ data }) => {
@@ -40,43 +29,41 @@ export const HeroCarouselBlockComponent: CmsComponent<
     <section className="hero">
       <div className="hero-content">
         <section className="flex flex-col">
-          <Slider {...settings}>
-            {utilityItems?.map((item, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className="flex relative flex-col items-start px-20 pt-14 pb-5 w-full min-h-[700px] max-md:px-5 max-md:max-w-full"
-                >
-                  <CmsImage
-                    loading="lazy"
-                    src={item?.BannerImage}
-                    alt="Inspirational background"
-                    className="object-cover absolute inset-0 size-full"
-                    layout="fill"
-                  />
-                  <div className="relative px-5 pt-5 pb-16 max-w-full bg-white bg-opacity-80 w-[604px]">
-                    <div className="flex gap-5 max-md:flex-col">
-                      <div className="flex flex-col w-[59%] max-md:ml-0 max-md:w-full">
-                        <h1 className="relative text-5xl text-teal-950 max-md:mt-10 max-md:text-4xl">
-                          {item.BannerTitle ?? ""}
-                        </h1>
-                      </div>
-                      <div className="flex flex-col ml-5 w-[41%] max-md:ml-0 max-md:w-full">
-                        <CmsEditable
-                          as={RichText}
-                          cmsFieldName="TileDescription"
-                          text={item.BannerDescription?.json}
-                        />
-                      </div>
+          {utilityItems?.map((item, idx) => {
+            return (
+              <div
+                key={idx}
+                className="flex relative flex-col items-start px-20 pt-14 pb-5 w-full min-h-[700px] max-md:px-5 max-md:max-w-full"
+              >
+                <CmsImage
+                  loading="lazy"
+                  src={item?.BannerImage}
+                  alt="Inspirational background"
+                  className="object-cover absolute inset-0 size-full"
+                  layout="fill"
+                />
+                <div className="relative px-5 pt-5 pb-16 max-w-full bg-white bg-opacity-80 w-[604px]">
+                  <div className="flex gap-5 max-md:flex-col">
+                    <div className="flex flex-col w-[59%] max-md:ml-0 max-md:w-full">
+                      <h1 className="relative text-5xl text-teal-950 max-md:mt-10 max-md:text-4xl">
+                        {item.BannerTitle ?? ""}
+                      </h1>
+                    </div>
+                    <div className="flex flex-col ml-5 w-[41%] max-md:ml-0 max-md:w-full">
+                      <CmsEditable
+                        as={RichText}
+                        cmsFieldName="TileDescription"
+                        text={item.BannerDescription?.json}
+                      />
                     </div>
                   </div>
-                  <button className="overflow-hidden relative z-10 px-12 py-7 mt-0 ml-44 text-sm font-bold tracking-wide text-white uppercase bg-cyan-900 max-md:px-5 max-md:ml-2.5">
-                    {item.BannerButtonText}
-                  </button>
                 </div>
-              );
-            })}
-          </Slider>
+                <button className="overflow-hidden relative z-10 px-12 py-7 mt-0 ml-44 text-sm font-bold tracking-wide text-white uppercase bg-cyan-900 max-md:px-5 max-md:ml-2.5">
+                  {item.BannerButtonText}
+                </button>
+              </div>
+            );
+          })}
         </section>
       </div>
     </section>
