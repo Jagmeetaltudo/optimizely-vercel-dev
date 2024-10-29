69c78ba09ab073213968c33ac298e014abdce493
Jagmeet Kaur <jagmeet.kaur@altudo.co>
2024-10-29 12:00:38 -0500

    AB Testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,43 @@
-import { type PropsWithChildren } from "react"
-import type { Metadata } from "next"
+import { type PropsWithChildren } from "react";
+import type { Metadata } from "next";
 
 // Components
-import { MoseyBankHeader } from '@/components/header'
-import { MoseyBankFooter } from '@/components/footer'
-import { ThemeProvider, Body } from '@/components/theme'
+import { MoseyBankHeader } from "@/components/header";
+import { MoseyBankFooter } from "@/components/footer";
+import { ThemeProvider, Body } from "@/components/theme";
 
 // Styling
-import { Figtree } from "next/font/google"
-import "./globals.scss"
+import { Figtree } from "next/font/google";
+import "./globals.scss";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    description: "An Optimizely website",
-    keywords: "Altudo, Accelerator",
-    title: {
-        default: "Altudo - An Optimizely Demo",
-        template: "%s - An Optimizely Demo"
-    }
+  description: "An Optimizely website",
+  keywords: "Altudo, Accelerator",
+  title: {
+    default: "Altudo - An Optimizely Demo",
+    template: "%s - An Optimizely Demo",
+  },
 };
 
-type RootLayoutProps = Readonly<PropsWithChildren<{}>>
+type RootLayoutProps = Readonly<PropsWithChildren<{}>>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
-    return <html lang="en">
-        <ThemeProvider value={{ theme: "system" }}>
-            <Body >
-                <div>
-                    {/* <MoseyBankHeader /> */}
-                    {children}
-                    {/* <MoseyBankFooter /> */}
-                </div>
-            </Body>
-        </ThemeProvider>
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.optimizely.com/js/6697360307257344.js" async></script>
+      </head>
+      <ThemeProvider value={{ theme: "system" }}>
+        <Body>
+          <div>
+            {/* <MoseyBankHeader /> */}
+            {children}
+            {/* <MoseyBankFooter /> */}
+          </div>
+        </Body>
+      </ThemeProvider>
     </html>
+  );
 }
